3ab3ff4d29bb0bc2cf4af6aaa76d9efafd9b0115
Sun Qi <745356675@qq.com>
2023-09-30 19:04:28 +0800

    feat: vkrhi init

--- source/runtime/modules/render/include/rhi/vulkan/VulkanRHI.h
+++ source/runtime/modules/render/include/rhi/vulkan/VulkanRHI.h
@@ -3,15 +3,129 @@
 #include <string>
 #include "rhi/RHI.h"
 #include <vulkan.h>
-class IVulkanRHI : public RHI{
+class IVulkanRHI : public RHI {
+public:
+    void Initialize() override {}
+    void ShutDown() override {}
+    const char* GetName() override { return "VulkanRHI"; }
 
+#pragma region resources creation
+    RHISamplerRef RHICreateSampler(const RHISamplerInitializer& _initializer) override {}
+    RHIRasterizationStateRef RHICreateRasterizationState(const RHIRasterizationStateInitializer& _init) override {}
+    RHIDepthStencilStateRef RHICreateDepthStencilState(const RHIDepthStencilStateInitializer& _init) override {}
+    RHIMultisampleStateRef RHICreateMultiSampleState(const RHIMultisampleStateInitializer& _init) override {}
+    RHIBlendStateRef RHICreateBlendState(const RHIBlendStateInitializer& _init) override {}
+    RHIVertexInputStateRef RHICreateVertexInputState(const VertexInputStateInitializerList& _init) override {}
 
+    RHIVertexShaderRef RHICreateVertexShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override {}
+    RHIFragmentShaderRef RHICreateFragmentShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override {}
+    RHIGeometryShaderRef RHICreateGeometryShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override {}
 
+    RHIMeshShaderRef RHICreateMeshShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override {}
+    RHIAmplificationShaderRef RHICreateAmplificationShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override {}
 
+    RHIComputeShaderRef RHICreateComputeShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override {}
 
+    RHIShaderLibraryRef RHICreateShaderLibrary(EShaderPlatform _platform, const std::string& _file_path, const std::string& name) override {}
+
+    RHIFenceRef RHICreateFence(const std::string& name) override {}
+
+    /* create cpu visible buffer for direct data transfer */
+    RHIStagingBufferRef RHICreateStagingBuffer() override {}
+
+    RHIShaderBoundStateRef RHICreateShaderBoundStage(
+        RHIVertexInputState* _vertex_input,
+        RHIVertexShader*     _vertex_shader,
+        RHIFragmentShader*   _fragment_shader,
+        RHIGeometryShader*   _geometry_shader) override {}
+
+    RHIGraphicsPipelineStateRef RHICreateGraphicsPipelineState(const RHIGraphicsPipelineStateInitializer& _init) override {}
+
+    RHIComputePipelineStateRef RHICreateComputePipelineState(RHIComputeShader* _compute_shader) override {}
+
+    RHIUniformBufferRef RHICreateUniformBuffer(const void* data, const RHIUniformBufferLayout* layout, EBufferUsageFlags _usage) override {}
+
+    void RHICopyBuffer(RHIBuffer* _src, RHIBuffer* _dst) override {}
+
+    RHIBufferRef RHICreateBuffer(const RHIBufferCreateInfo& info) override {}
+
+    RHIShaderResourceViewRef  RHICreateShaderResourceView(RHIViewableResource* _resource, const RHIViewInfo& _view_info) override {}
+    RHIUnorderedAccessViewRef RHICreateUnorderedAccessView(RHIViewableResource* _resource, const RHIViewInfo& _view_info) override {}
+
+#pragma endregion
 };
 
-class VulkanRHIImpl : IVulkanRHI{
+class VulkanRHIImpl : IVulkanRHI {
+public:
 
+    /**
+     * @brief Setup the vulkan instance, enable required extensions and connect to the physical device (GPU).
+     */
+    void Initialize() override;
+
+    /**
+     * @brief Clean up the vulkan instance.
+     */
+    void ShutDown() override;
+
+    /**
+     * @brief Get the name of the RHI.
+     * @return The name of the RHI
+     */
+    const char* GetName() override;
+
+#pragma region resources creation
+    RHISamplerRef RHICreateSampler(const RHISamplerInitializer& _initializer) override;
+    RHIRasterizationStateRef RHICreateRasterizationState(const RHIRasterizationStateInitializer& _init) override;
+    RHIDepthStencilStateRef RHICreateDepthStencilState(const RHIDepthStencilStateInitializer& _init) override;
+    RHIMultisampleStateRef RHICreateMultiSampleState(const RHIMultisampleStateInitializer& _init) override;
+    RHIBlendStateRef RHICreateBlendState(const RHIBlendStateInitializer& _init) override;
+    RHIVertexInputStateRef RHICreateVertexInputState(const VertexInputStateInitializerList& _init) override;
+
+    RHIVertexShaderRef RHICreateVertexShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override;
+    RHIFragmentShaderRef RHICreateFragmentShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override;
+    RHIGeometryShaderRef RHICreateGeometryShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override;
+
+    RHIMeshShaderRef RHICreateMeshShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override;
+    RHIAmplificationShaderRef RHICreateAmplificationShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override;
+
+    RHIComputeShaderRef RHICreateComputeShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) override;
+
+    RHIShaderLibraryRef RHICreateShaderLibrary(EShaderPlatform _platform, const std::string& _file_path, const std::string& name) override;
+
+    RHIFenceRef RHICreateFence(const std::string& name) override;
+
+    /* create cpu visible buffer for direct data transfer */
+    RHIStagingBufferRef RHICreateStagingBuffer() override;
+
+    RHIShaderBoundStateRef RHICreateShaderBoundStage(
+        RHIVertexInputState* _vertex_input,
+        RHIVertexShader*     _vertex_shader,
+        RHIFragmentShader*   _fragment_shader,
+        RHIGeometryShader*   _geometry_shader) override;
+
+    RHIGraphicsPipelineStateRef RHICreateGraphicsPipelineState(const RHIGraphicsPipelineStateInitializer& _init) override;
+
+    RHIComputePipelineStateRef RHICreateComputePipelineState(RHIComputeShader* _compute_shader) override;
+
+    RHIUniformBufferRef RHICreateUniformBuffer(const void* data, const RHIUniformBufferLayout* layout, EBufferUsageFlags _usage) override;
+
+    void RHICopyBuffer(RHIBuffer* _src, RHIBuffer* _dst) override;
+
+    RHIBufferRef RHICreateBuffer(const RHIBufferCreateInfo& info) override;
+
+    RHIShaderResourceViewRef RHICreateShaderResourceView(RHIViewableResource* _resource, const RHIViewInfo& _view_info) override;
+    RHIUnorderedAccessViewRef RHICreateUnorderedAccessView(RHIViewableResource* _resource, const RHIViewInfo& _view_info) override;
+#pragma endregion
+
+private:
+    VkInstance m_instance;
+    VkPhysicalDevice m_physical_device;
+
+#pragma region helper functions
+private:
+    VkResult CreateInstance(bool _enable_validation);
+
+#pragma endregion
 };
 #endif// !VULKAN_RHI_H
--- source/runtime/modules/render/source/rhi/vulkan/VulkanRHI.cpp
+++ source/runtime/modules/render/source/rhi/vulkan/VulkanRHI.cpp
@@ -1,7 +1,83 @@
 #include "misc/MacroUtils.h"
 #include "rhi/vulkan/VulkanRHI.h"
+#include "vulkan/config.h"
 
 #include <string>
 #include <vulkan.h>
 
-std::string vk_layer = MACRO_STR(__ENGINE_NAME__)MACRO_STR(_VK_LAYER_PATH);
+const std::string vk_layer = MACRO_STR(__ENGINE_NAME__)MACRO_STR(_VK_LAYER_PATH);
+
+void VulkanRHIImpl::Initialize() {
+
+}
+
+void VulkanRHIImpl::ShutDown() {}
+
+const char* VulkanRHIImpl::GetName() {
+    return "VulkanRHIImplementation";
+}
+
+#pragma region resources creation
+RHISamplerRef VulkanRHIImpl::RHICreateSampler(const RHISamplerInitializer& _initializer) {}
+RHIRasterizationStateRef VulkanRHIImpl::RHICreateRasterizationState(const RHIRasterizationStateInitializer& _init) {}
+RHIDepthStencilStateRef VulkanRHIImpl::RHICreateDepthStencilState(const RHIDepthStencilStateInitializer& _init) {}
+RHIMultisampleStateRef VulkanRHIImpl::RHICreateMultiSampleState(const RHIMultisampleStateInitializer& _init) {}
+RHIBlendStateRef VulkanRHIImpl::RHICreateBlendState(const RHIBlendStateInitializer& _init) {}
+RHIVertexInputStateRef VulkanRHIImpl::RHICreateVertexInputState(const VertexInputStateInitializerList& _init) {}
+
+RHIVertexShaderRef VulkanRHIImpl::RHICreateVertexShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) {}
+RHIFragmentShaderRef VulkanRHIImpl::RHICreateFragmentShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) {}
+RHIGeometryShaderRef VulkanRHIImpl::RHICreateGeometryShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) {}
+
+RHIMeshShaderRef VulkanRHIImpl::RHICreateMeshShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) {}
+RHIAmplificationShaderRef VulkanRHIImpl::RHICreateAmplificationShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) {}
+
+RHIComputeShaderRef VulkanRHIImpl::RHICreateComputeShader(std::vector<const uint8_t> _code, const SHA256Hash& _hash) {}
+
+RHIShaderLibraryRef VulkanRHIImpl::RHICreateShaderLibrary(EShaderPlatform _platform, const std::string& _file_path, const std::string& name) {}
+
+RHIFenceRef VulkanRHIImpl::RHICreateFence(const std::string& name) {}
+
+/* create cpu visible buffer for direct data transfer */
+RHIStagingBufferRef VulkanRHIImpl::RHICreateStagingBuffer() {}
+
+RHIShaderBoundStateRef VulkanRHIImpl::RHICreateShaderBoundStage(
+    RHIVertexInputState* _vertex_input,
+    RHIVertexShader* _vertex_shader,
+    RHIFragmentShader* _fragment_shader,
+    RHIGeometryShader* _geometry_shader) {}
+
+RHIGraphicsPipelineStateRef VulkanRHIImpl::RHICreateGraphicsPipelineState(const RHIGraphicsPipelineStateInitializer& _init) {}
+
+RHIComputePipelineStateRef VulkanRHIImpl::RHICreateComputePipelineState(RHIComputeShader* _compute_shader) {}
+
+RHIUniformBufferRef  VulkanRHIImpl::RHICreateUniformBuffer(const void* data, const RHIUniformBufferLayout* layout, EBufferUsageFlags _usage) {}
+
+void VulkanRHIImpl::RHICopyBuffer(RHIBuffer* _src, RHIBuffer* _dst) {}
+
+RHIBufferRef VulkanRHIImpl::RHICreateBuffer(const RHIBufferCreateInfo& info) {}
+
+RHIShaderResourceViewRef VulkanRHIImpl::RHICreateShaderResourceView(RHIViewableResource* _resource, const RHIViewInfo& _view_info) {}
+RHIUnorderedAccessViewRef VulkanRHIImpl::RHICreateUnorderedAccessView(RHIViewableResource* _resource, const RHIViewInfo& _view_info) {}
+#pragma endregion
+
+#pragma region helper functions
+VkResult VulkanRHIImpl::CreateInstance(bool _enable_validation) {
+    VkApplicationInfo applicationInfo{};
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    // applicationInfo.pApplicationName = MACRO_STR(__ENGINE_NAME__);
+    // applicationInfo.applicationVersion = VK_MAKE_VERSION(PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH);
+    applicationInfo.pEngineName = MACRO_STR(__ENGINE_NAME__);
+    applicationInfo.engineVersion = VK_MAKE_VERSION(PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH);
+    applicationInfo.apiVersion = VK_API_VERSION_1_3;
+
+    VkInstanceCreateInfo instanceCreateInfo{};
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pApplicationInfo = &applicationInfo;
+
+}
+
+#pragma endregion
+
+
+
