8d54b8d9b6ded22cf3d92192234ed5385cc8c1cd
JohnW1ck <191250141@smail.nju.edu.cn>
2023-10-02 23:01:11 +0800

    shader hash functions

---
+++ source/runtime/modules/render/source/shader/ShaderCommon.cpp
@@ -0,0 +1,19 @@
+#include "ShaderCommon.h"
+#include "string_view"
+void ShaderCompilerOutput::GenerateCompiledHash() {
+    Hash64City& hash = compiled_hash;
+    hash.Update(shader_code.data(), shader_code.size());
+
+    auto param_map = parameter_map.GetShaderParameterMap();
+    for(const auto& param : param_map){
+        const auto& name = param.first;
+        const auto& param_value = param.second;
+        hash.Update(name.data(), name.length());
+        hash.Update((const char*)(&param_value.type), sizeof(EShaderParameterType));
+        hash.Update((const char*)(&param_value.buffer_index), sizeof(uint16_t));
+        hash.Update((const char*)(&param_value.slot), sizeof(uint16_t));
+        hash.Update((const char*)(&param_value.size), sizeof(uint16_t));
+    }
+
+
+}
---
+++ source/runtime/modules/render/source/shader/ShaderResource.cpp
@@ -0,0 +1,17 @@
+#include "ShaderResource.h"
+void ShaderCodeResourceMap::AddShaderCompilerOutput(const ShaderCompilerOutput& _output) {
+
+    const Hash64City& compiled_hash = _output.compiled_hash;
+    const auto& shader_code = _output.shader_code;
+
+    auto pos = std::lower_bound(shader_hashes.begin(), shader_hashes.end(), compiled_hash);
+    if(pos == shader_hashes.end()){
+        shader_hashes.push_back(compiled_hash);
+        shader_entries.emplace_back(shader_code, _output.target_info.shader_type);
+    }
+}
+int32_t ShaderCodeResourceMap::GetIndexByHash(const Hash64City& _hash) {
+    auto pos = std::lower_bound(shader_hashes.begin(), shader_hashes.end(), _hash);
+    if(pos == shader_hashes.end())return -1;
+    return pos - shader_hashes.begin();
+}
