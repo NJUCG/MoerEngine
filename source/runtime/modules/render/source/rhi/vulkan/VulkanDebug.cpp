fe71fd4f07ebd4203c6b64a47d564f99930e4501
Sun Qi <745356675@qq.com>
2023-10-12 15:12:03 +0800

    feat(sq): add instance creation validation msg

--- source/runtime/modules/render/source/rhi/vulkan/VulkanDebug.cpp
+++ source/runtime/modules/render/source/rhi/vulkan/VulkanDebug.cpp
@@ -46,13 +46,7 @@ namespace Vulkan {
         }
 
         VkDebugUtilsMessengerCreateInfoEXT debug_utils_messenger_create_info{};
-        debug_utils_messenger_create_info.sType           = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-        debug_utils_messenger_create_info.pNext           = nullptr;
-        debug_utils_messenger_create_info.flags           = 0;
-        debug_utils_messenger_create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-        debug_utils_messenger_create_info.messageType     = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-        debug_utils_messenger_create_info.pfnUserCallback = DebugCallback;
-        debug_utils_messenger_create_info.pUserData       = nullptr;
+        PopulateDebugMessengerCreateInfo(debug_utils_messenger_create_info);
 
         assert(vkCreateDebugUtilsMessengerEXT(instance, &debug_utils_messenger_create_info, nullptr, &debug_utils_messenger) == VK_SUCCESS);
     }
@@ -61,6 +55,18 @@ namespace Vulkan {
         if (debug_utils_messenger != VK_NULL_HANDLE) {
             vkDestroyDebugUtilsMessengerEXT(instance, debug_utils_messenger, nullptr);
         }
+    }
+
+    void Debug::PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& create_info) {
+        create_info = {};
+
+        create_info.sType           = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        create_info.pNext           = nullptr;
+        create_info.flags           = 0;
+        create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        create_info.messageType     = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        create_info.pfnUserCallback = DebugCallback;
+        create_info.pUserData       = nullptr;
     }
 
     PFN_vkCmdBeginDebugUtilsLabelEXT  DebugUtils::vkCmdBeginDebugUtilsLabelEXT  = nullptr;
