15d77c5cc5d44acab4e26085660f8ace5d7f9fdf
ChenWang <1401520906@qq.com>
2023-10-20 19:11:16 +0800

    add mitsuba xml loader

---
+++ source/runtime/modules/resource/include/loader/mixml/Parser.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <filesystem>
+
+namespace Moer::Resource {
+namespace MiXml {
+    struct Object;
+
+    class Parser {
+    public:
+        Parser() noexcept;
+        ~Parser() noexcept;
+
+        /**
+         * @param file_path the absolute file path of mitsuba-style scene file
+         * @return the root xml obj of the target scene(tree struction)
+        */
+        Object* LoadFromFile(std::filesystem::path file_path) noexcept;
+
+    private:
+        struct Impl;
+        Impl* m_impl = nullptr;
+    };
+}
+}// namespace Moer::Resource::MiXml
