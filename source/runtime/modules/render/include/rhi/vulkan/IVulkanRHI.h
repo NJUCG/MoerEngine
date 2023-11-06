adea6e1f4ee5ac1caa744463d7de95f9682ea658
Sun Qi <745356675@qq.com>
2023-11-06 19:54:09 +0800

    feat(sq): delete `RHICreateShader`, add `VulkanRHICommandQueue` constructor

--- source/runtime/modules/render/include/rhi/vulkan/IVulkanRHI.h
+++ source/runtime/modules/render/include/rhi/vulkan/IVulkanRHI.h
@@ -65,8 +65,6 @@ public:
     RHIGraphicsCommandList* CreateGraphicsCommandList(RHIGraphicsPipelineState* _initial_state = nullptr) override { return nullptr; }
     RHIComputeCommandList*  CreateComputeCommandList(RHIComputePipelineState* _initial_state = nullptr) override { return nullptr; }
 
-    RHIShaderRef RHICreateShader(Shader*) override { return RHIShaderRef{}; }
-
 #pragma endregion
 
 #pragma region Viewport
