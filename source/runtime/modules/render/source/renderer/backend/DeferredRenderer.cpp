70574cb225d2d7cf6141e529cb3c1a1f0121108c
JohnW1ck <1715288914@qq.com>
2024-03-19 14:49:14 +0800

    fix unnecessary draw cmd

--- source/runtime/modules/render/source/renderer/backend/DeferredRenderer.cpp
+++ source/runtime/modules/render/source/renderer/backend/DeferredRenderer.cpp
@@ -122,6 +122,7 @@ namespace Moer {
 
         void PrePass();
         void RecheckPass();
+        void LightingPass();
 
         void DispatchDrawScene(const CameraData& _camera_data, bool _first_pass = true);
 
@@ -607,40 +608,8 @@ namespace Moer {
 
         EnqueueRenderTask(std::move(sync_command_list));
         EnqueueRenderTask(std::move(reset_counter_buffer));
-        // EnqueueRenderTask(std::move(prepass_cull_task));
 
         PrePass();
-
-        // EnqueueRenderTask([this]() {
-        //     auto                    info         = virtual_viewport->GetBackBufferInfo();
-        //     RHIUAVRef               uav          = info.backbuffer_uav;
-        //     uint32_t                frame_offset = frame_counter % render_cmd_lists.size();
-        //     RHIGraphicsCommandList* cmd_list     = render_cmd_lists[frame_offset];
-        //
-        //     RHIBarrierDependencyInfo barrier_dependency_info;
-        //     barrier_dependency_info.texture_barriers.resize(2);
-        //     auto& texture_barrier_info = barrier_dependency_info.texture_barriers[0];
-        //     texture_barrier_info
-        //         .SetTexture(uav->GetTexture())
-        //         .SetDstTextureLayout(TEXTURE_LAYOUT_COLOR_ATTACHMENT)
-        //         .SetSrcTextureLayout(TEXTURE_LAYOUT_TRANSFER_SRC)
-        //         .SetSrcStage(ERHIPipelineStageFlags::PS_TRANSFER)
-        //         .SetDstStage(ERHIPipelineStageFlags::PS_COLOR_ATTACHMENT_OUTPUT)
-        //         .SetDstAccessFlags(ERHIAccessFlags::COLOR_ATTACHMENT_WRITE);
-        //
-        //     auto& depth_barrier = barrier_dependency_info.texture_barriers[1];
-        //     depth_barrier
-        //         .SetTexture(depth_buffer[frame_offset])
-        //         .SetDstTextureLayout(TEXTURE_LAYOUT_DEPTH_STENCIL_WRITE)
-        //         .SetSrcTextureLayout(TEXTURE_LAYOUT_UNDEFINED)
-        //         .SetSubResourceRange(RHISubresourceRange(ETextureAspectFlags::DEPTH_SLICE | ETextureAspectFlags::STENCIL_SLICE))
-        //         .SetSrcStage(ERHIPipelineStageFlags::PS_COMPUTE_SHADER)
-        //         .SetDstStage(ERHIPipelineStageFlags::PS_EARLY_FRAGMENT_TESTS)
-        //         .SetSrcAccessFlags(ERHIAccessFlags::SHADER_READ)
-        //         .SetDstAccessFlags(ERHIAccessFlags::DEPTH_STENCIL_READ | ERHIAccessFlags::DEPTH_STENCIL_WRITE);
-        //
-        //     cmd_list->SetPipelineBarrier(barrier_dependency_info);
-        // });
 
         DispatchDrawScene(camera_data);
 
@@ -650,47 +619,15 @@ namespace Moer {
             HiZBuilder::GetInstance().DispatchBuildHiZ(render_cmd_lists[frame_offset], depth_buffer_srv[frame_offset], hiz_buffer);
         });
         RecheckPass();
-        // EnqueueRenderTask([this]() {
-        //     auto                    info         = virtual_viewport->GetBackBufferInfo();
-        //     RHIUAVRef               uav          = info.backbuffer_uav;
-        //     uint32_t                frame_offset = frame_counter % render_cmd_lists.size();
-        //     RHIGraphicsCommandList* cmd_list     = render_cmd_lists[frame_offset];
-        //
-        //     RHIBarrierDependencyInfo barrier_dependency_info;
-        //     barrier_dependency_info.texture_barriers.resize(2);
-        //     auto& texture_barrier_info = barrier_dependency_info.texture_barriers[0];
-        //     texture_barrier_info
-        //         .SetTexture(uav->GetTexture())
-        //         .SetDstTextureLayout(TEXTURE_LAYOUT_COLOR_ATTACHMENT)
-        //         .SetSrcTextureLayout(TEXTURE_LAYOUT_COLOR_ATTACHMENT)
-        //         .SetSrcStage(ERHIPipelineStageFlags::PS_COLOR_ATTACHMENT_OUTPUT)
-        //         .SetDstStage(ERHIPipelineStageFlags::PS_COLOR_ATTACHMENT_OUTPUT)
-        //         .SetDstAccessFlags(ERHIAccessFlags::COLOR_ATTACHMENT_WRITE)
-        //         .SetSrcAccessFlags(ERHIAccessFlags::COLOR_ATTACHMENT_WRITE);
-        //
-        //     auto& depth_barrier = barrier_dependency_info.texture_barriers[1];
-        //     depth_barrier
-        //         .SetTexture(depth_buffer[frame_offset])
-        //         .SetDstTextureLayout(TEXTURE_LAYOUT_DEPTH_STENCIL_WRITE)
-        //         .SetSrcTextureLayout(TEXTURE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
-        //         .SetSubResourceRange(RHISubresourceRange(ETextureAspectFlags::DEPTH_SLICE | ETextureAspectFlags::STENCIL_SLICE))
-        //         .SetSrcStage(ERHIPipelineStageFlags::PS_COMPUTE_SHADER)
-        //         .SetDstStage(ERHIPipelineStageFlags::PS_EARLY_FRAGMENT_TESTS)
-        //         .SetSrcAccessFlags(ERHIAccessFlags::SHADER_READ)
-        //         .SetDstAccessFlags(ERHIAccessFlags::DEPTH_STENCIL_READ | ERHIAccessFlags::DEPTH_STENCIL_WRITE);
-        //
-        //     cmd_list->SetPipelineBarrier(barrier_dependency_info);
-        // });
+
         DispatchDrawScene(camera_data, false);
         EnqueueRenderTask([this]() {
             auto frame_offset = frame_counter % render_cmd_lists.size();
             HiZBuilder::GetInstance().DispatchBuildHiZ(render_cmd_lists[frame_offset], depth_buffer_srv[frame_offset], hiz_buffer);
         });
-        //instance cull, output instance recheck buffer and instance-meshlet mapping
-        //meshlet cull, output instance-meshlet recheck mapping buffer and draw indirect buffer
-        //draw indirect
-        //instance recheck, append instance-meshlet recheck mapping
-        //meshlet recheck
+
+        LightingPass();
+
         EnqueueRenderTask([this]() {
             RHIGraphicsCommandList* cmd_list = render_cmd_lists[frame_counter % render_cmd_lists.size()];
 
@@ -1026,77 +963,9 @@ namespace Moer {
     }
 
     void DeferredRenderer::Impl::DispatchDrawScene(const CameraData& _camera_data, bool _first_pass) {
-
-        // TestDeferredTriangleShaderVert::Parameters params;
-        // params.camera_data   = _camera_data;
-        // params.instance_data = instance_buffer_view;
-        //
-        // RHIBatchedShaderParameters batched_params;
-        // batched_params.SetParameters(ShaderResourceManager::GetInstance().GetShader<TestDeferredTriangleShaderVert>(), params);
-        //
-        // auto draw_indirect = [this, batched_params = (batched_params), first_pass(_first_pass)]() {
-        //     auto      info = virtual_viewport->GetBackBufferInfo();
-        //     RHIUAVRef uav  = info.backbuffer_uav;
-        //
-        //     uint32_t                frame_offset = frame_counter % render_cmd_lists.size();
-        //     RHIGraphicsCommandList* cmd_list     = render_cmd_lists[frame_counter % render_cmd_lists.size()];
-        //
-        //     RHIRenderPassInfo pass_info{};
-        //
-        //     pass_info.color_attachments[0].color_attachment_action = first_pass ? AC_CLEAR_STORE : AC_LOAD_STORE;
-        //     RenderAttachmentView& render_attachment_view           = pass_info.color_attachments[0].color_attachment_view;
-        //
-        //     render_attachment_view.required_layout  = TEXTURE_LAYOUT_COLOR_ATTACHMENT;
-        //     render_attachment_view.texture_view     = uav;
-        //     render_attachment_view.clear_attachment = RHIClearAttachment(EClearAttachment::COLOR);
-        //
-        //     auto& depth_attachment_view        = pass_info.depth_stencil_attachment.depth_stencil_attachment_view;
-        //     depth_attachment_view.texture_view = depth_buffer_uav[frame_offset];
-        //
-        //     depth_attachment_view.required_layout                   = TEXTURE_LAYOUT_DEPTH_STENCIL_WRITE;
-        //     depth_attachment_view.clear_attachment                  = RHIClearAttachment::Preset<RHIConfig::ClearMode::DEPTH_STENCIL>();
-        //     pass_info.depth_stencil_attachment.depth_stencil_action = first_pass ? AC_CLEAR_STORE : AC_LOAD_STORE;
-        //
-        //     Extent3D extent              = uav->GetTexture()->GetExtent3D();
-        //     pass_info.render_area.extent = Extent2D(extent.x, extent.y);
-        //     pass_info.render_area.offset = Offset2D(0, 0);
-        //
-        //     cmd_list->BeginRenderPass(pass_info, "Draw Meshlets");
-        //
-        //     const VirtualViewportInfo& viewport_info = virtual_viewport->GetInfo();
-        //     ViewPort                   viewport{0, 0, float(viewport_info.extent.x), float(viewport_info.extent.y), 0, 1};
-        //     cmd_list->SetViewPort(viewport);
-        //     cmd_list->SetScissor({0, 0, uint32_t(viewport_info.extent.x), uint32_t(viewport_info.extent.y)});
-        //
-        //     cmd_list->SetPipelineState(pipeline_state);
-        //     g_rhi->RHISetBatchedShaderParameters(pipeline_state, batched_params);
-        //
-        //     auto* const scene = g_scene;
-        //     if (scene) {
-        //         // Shader* vert_shader = ShaderResourceManager::GetShader<TestDeferredTriangleShaderVert>();
-        //         cmd_list->BindIndexBuffer(scene->GetBuffer("index_buffer"), 0, IET_UINT32);
-        //         uint32_t           offset[]           = {0, 0};
-        //         const RHIBufferRef prim_vertex_buffer = scene->GetBuffer("vertex_buffer");
-        //         const RHIBufferRef instance_id_buffer = scene->GetBuffer("instance_id_buffer");
-        //         RHIBufferRef       vbuffers[]         = {prim_vertex_buffer, instance_id_buffer};
-        //         cmd_list->BindVertexBuffers(0, 2, vbuffers, offset);
-        //         auto draw_cnt_offset = first_pass ? draw_count_offset : recheck_draw_count_offset;
-        //         cmd_list->DrawIndexedIndirect(draw_indirect_buffer, 0, draw_count_buffer, draw_cnt_offset, draw_indirect_buffer->GetNumElement(), sizeof(DrawInstanceCmd));
-        //     } else {
-        //         assert(false);
-        //     }
-        //
-        //     cmd_list->EndRenderPass();
-        // };
-        //
-        // EnqueueRenderTask(std::move(draw_indirect));
-        
-         EnqueueRenderTask([&] {
+        auto dispatch_gbuffer_pass = [&, b_first_pass(_first_pass)]() {
             RenderGraph render_graph;
-            auto        cmd_list = render_cmd_lists[frame_counter % render_cmd_lists.size()];
-            //  cmd_list->Reset();
-
-            //  cmd_list->BeginRecording();
+            auto*       cmd_list = render_cmd_lists[frame_counter % render_cmd_lists.size()];
 
             {
                 Extent3D extent = virtual_viewport->GetNextBackBufferExtent();
@@ -1107,11 +976,15 @@ namespace Moer {
                         auto normal = render_graph.CreateTexture("normal", {.extent2D = Extent2D(extent.x, extent.y), .format = EPixelFormat::PF_R8G8B8A8_UNORM, .usage = ETextureUsageFlags::COLOR_ATTACHMENT | ETextureUsageFlags::SAMPLED});
                         auto mat    = render_graph.CreateTexture("mat", {.extent2D = Extent2D(extent.x, extent.y), .format = EPixelFormat::PF_R32_UINT, .usage = ETextureUsageFlags::COLOR_ATTACHMENT | ETextureUsageFlags::SAMPLED});
                         auto depth  = render_graph.ImportTexture("depth",depth_buffer[frame_counter % render_cmd_lists.size()] );
+                        if (!b_first_pass) {
+                            builder.readTextures({normal, mat}, RenderGraphTexture::Usage::COLOR_ATTACHMENT);
+                            builder.readTexture(depth, RenderGraphTexture::Usage::DEPTH_STENCIL_ATTACHMENT);
+                        }
                         builder.writeTextures({normal, mat}, RenderGraphTexture::Usage::COLOR_ATTACHMENT);
                         builder.writeTexture(depth, RenderGraphTexture::Usage::DEPTH_STENCIL_ATTACHMENT);
                         builder.DeclareRenderPass({.color_attachments = {mat, normal}, .depth_stencil_attachment = depth});
                     },
-                    [this](RenderPassContext& context) {
+                    [this, b_first_pass](RenderPassContext& context) {
                         auto cmd_list = context.cmd_list;
                         cmd_list->SetPipelineState(gbuffer_pipeline_state);
                         // cmd_list->BindVertexBuffers()
@@ -1152,8 +1025,8 @@ namespace Moer {
                         //     auto mesh_info = RenderableManager::Get().GetMeshInfo(entity);
                         //     cmd_list->DrawIndexedInstanced(mesh_info.index_count, 1, mesh_info.index_offset, mesh_info.vertex_offset, instance_idx++);
                         // });
-                        
-                        cmd_list->DrawIndexedIndirect(draw_indirect_buffer, 0, draw_count_buffer, draw_count_offset, 5000, sizeof(DrawInstanceCmd));
+                        auto draw_cnt_offset = b_first_pass ? draw_count_offset : recheck_draw_count_offset;
+                        cmd_list->DrawIndexedIndirect(draw_indirect_buffer, 0, draw_count_buffer, draw_cnt_offset, draw_indirect_buffer->GetNumElement(), sizeof(DrawInstanceCmd));
                     });
 
                 render_graph.AddGraphicPass(
@@ -1215,11 +1088,76 @@ namespace Moer {
             }
 
             render_graph.Execute({cmd_list, virtual_viewport->GetNextBackBufferExtent()});
-    });
-
+        };
+        EnqueueRenderTask(std::move(dispatch_gbuffer_pass));
     }
 
     void DeferredRenderer::Impl::BasePass() {
+    }
+    void DeferredRenderer::Impl::LightingPass() {
+        auto dispatch_rdg = [&]() {
+            RenderGraph render_graph;
+            auto*       cmd_list = render_cmd_lists[frame_counter % render_cmd_lists.size()];
+            render_graph.AddGraphicPass(
+                "Lighting Pass", [&](RenderGraph::Builder& builder) {
+            auto normal = render_graph.GetBlackBoard().GetHandle("normal");
+            auto mat = render_graph.GetBlackBoard().GetHandle("mat");
+                        auto depth = render_graph.GetBlackBoard().GetHandle("depth");
+            // auto position = render_graph.GetBlackBoard().GetHandle("position");
+            auto output = render_graph.ImportTexture("swapchain_output",virtual_viewport->GetBackBufferInfo().backbuffer_uav->GetTexture());
+            builder.readTextures({normal,mat,depth},RenderGraphTexture::Usage::SAMPLED).writeTexture(output);
+            builder.DeclareRenderPass({.color_attachments =  {output}}); }, [&](RenderPassContext& context) {
+            auto* cmd_list = context.cmd_list;
+            cmd_list->SetPipelineState(lighting_pipeline_state);
+
+            //First Get all material instances
+            //Material organize this materials 
+            //Material bind all resources for it's pass
+            //Draw a full screen quad pass for each material type 
+            Moer::UnorderedSet<EMaterialType> material_types = {}; 
+            Moer::UnorderedMap<EMaterialType,Moer::Array<MaterialInstanceRef>> material_instances;
+            g_scene->ForEach([&](Entity entity) {
+                if (RenderableManager::Get().Contains(entity)) {
+
+                    auto mi = RenderableManager::Get().GetMaterialInstance(entity);
+                    material_types.insert(mi->GetMaterial()->GetType());
+                    material_instances[mi->GetMaterial()->GetType()].push_back(mi);
+                }
+            });
+
+            auto mat_srv    = render_graph.GetBlackBoard().GetTexture("mat")->GetSRV();
+            auto normal_srv = render_graph.GetBlackBoard().GetTexture("normal")->GetSRV();
+            auto depth_srv  = render_graph.GetBlackBoard().GetTexture("depth")->GetSRV();
+
+            Camera*      camera = CameraManager::Get().Get(g_scene->GetMainCamera());
+            LightingData lighting_data;
+            lighting_data.inv_view_proj = Inverse(camera->GetProjectionMatrix() * camera->GetViewMatrix());
+            lighting_data.light_count   = lights.size();
+               
+            for(auto type : material_types){
+                RHIBatchedShaderParameters parameters;
+                LightingShaderFrag::Parameters frag_params;
+                frag_params.lighting_data = lighting_data;
+                frag_params.light_data = light_buffer_view;
+                frag_params.depth_attach = depth_srv;
+                frag_params.normal_attach = normal_srv;
+                frag_params.mat_attach = mat_srv;
+                parameters.SetParameters(ShaderResourceManager::GetInstance().GetShader<LightingShaderFrag>(),frag_params);
+                
+                auto& mat_instances = material_instances[type];
+                MaterialRef material = mat_instances[0]->GetMaterial();
+                material->OrganizeInstancesAndBind(parameters,mat_instances);
+                g_rhi->RHISetBatchedShaderParameters(lighting_pipeline_state,parameters);
+
+                cmd_list->Draw(3, 1,0,0);
+                
+            } });
+
+            render_graph.SetGraphOutput(render_graph.GetBlackBoard().GetHandle("swapchain_output"));
+            render_graph.Execute({cmd_list, virtual_viewport->GetNextBackBufferExtent()});
+        };
+
+        // EnqueueRenderTask(std::move(dispatch_rdg)); //not ready yet
     }
 
 }// namespace Moer
