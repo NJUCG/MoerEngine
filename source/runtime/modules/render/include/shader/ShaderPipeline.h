38b9c87c38b94c10e0a409fc7ed1a2685211db3b
JohnW1ck <1715288914@qq.com>
2024-05-17 23:46:41 +0800

    template shader pipeline test

--- source/runtime/modules/render/include/shader/ShaderPipeline.h
+++ source/runtime/modules/render/include/shader/ShaderPipeline.h
@@ -1,7 +1,12 @@
 #ifndef MOER_SHADER_PIPELINE_H
 #define MOER_SHADER_PIPELINE_H
+#include "misc/STL.h"
 #include "rhi/RHIResource.h"
 #include <string_view>
+#include <tuple>
+#include <typeindex>
+#include <utility>
+#include <variant>
 struct TDummy {};
 #define BEGIN_SHADER_PARAMS \
     using InnerTArg = ShaderArgs < TDummy,
@@ -9,66 +14,91 @@ struct TDummy {};
 #define END_SHADER_PARAMS \
     > ;
 
-#define DEFINE_SHADER_TEXTURE(name) \
-    , ShaderArg<RHITextureRef, #name>
+#define DEFINE_SHADER_TEX(name) \
+    StringType(name) using name = ShaderArg<RHITextureRef, GetStringType(name)>
 
 #define DEFINE_SHADER_BUFFER(name) \
-    , ShaderArg<RHIBufferRef, #name>
+    StringType(name) using name = ShaderArg<RHIBufferRef, GetStringType(name)>
 
-#define ShaderArgType(type, in_name)                       \
-    struct in_name {                                       \
-        using Type                             = type;     \
+#define DEFINE_SHADER_ARGS(...) \
+    using InnerArgs = ShaderArgs<__VA_ARGS__>
+
+#define StringType(in_name)                                \
+    struct String##in_name {                               \
         static constexpr std::string_view name = #in_name; \
-        static const std::string_view     GetName() {      \
-            return name;                               \
+        static constexpr std::string_view GetName() {      \
+            return name;                                   \
         }                                                  \
-        size_t hash_code;                                  \
-        in_name() { hash_code = GetHash(name); }           \
     };
+
+#define GetStringType(name) \
+    String##name
 
 namespace Moer {
-    template<const char... Name>
-    struct StringType {
-        static constexpr char name[] = {Name..., '\0'};
-    };
-    template<typename T, StringType arg_name>
+
+    template<typename T, typename arg_name>
     struct ShaderArg {
-        static constexpr char* name = arg_name;
-        size_t                 hash;
+        static constexpr std::string_view name = arg_name::name;
+        using type                             = T;
         ShaderArg() {
-            hash = std::hash<char*>{}(name);
         }
         static const std::string_view GetName() {
             return name;
         }
+        static uint64 GetHashCode() {
+            return GetHash(name);
+        }
     };
+
+    struct BufferArg {};
+    struct TextureArg {};
+
     template<typename... Args>
     struct ShaderArgs {
-        static constexpr size_t size = sizeof...(Args);
-        using tuple_helper           = std::tuple<Args...>;
-        template<typename T, typename TArg>
-        void SetParam(T& t){};
+        static constexpr size_t arg_size = sizeof...(Args);
+        using tuple_helper               = std::tuple<Args...>;
 
-        template<typename... dst_args, std::size_t... Is>
-        void SetParams(std::tuple<dst_args...>& tuple, std::index_sequence<Is...>) {
-            //set arg for specific index in tuple_helper
-            (..., std::invoke([=, this](auto& item) {
-                SetParam<decltype(item), decltype(std::get<Is>(tuple_helper))>(item);
-            }(), std::get<Is>(tuple)));
-            
-            
+        template<typename T, typename Tuple>
+        struct Index;
+
+        template<typename T, typename... Types>
+        struct Index<T, std::tuple<T, Types...>> {
+            static const std::size_t value = 0;
+        };
+
+        template<typename T, typename U, typename... Types>
+        struct Index<T, std::tuple<U, Types...>> {
+            static const std::size_t value = 1 + Index<T, std::tuple<Types...>>::value;
+        };
+
+        template<typename T, typename TArg>
+        void SetParam(T&& _t) {
+            using cpp_type       = typename TArg::type;
+            constexpr auto index = Index<TArg, tuple_helper>::value;
+            if constexpr (std::is_same_v<cpp_type, TextureArg>) {
+                //do texture stuff
+                if constexpr (std::is_same_v<T, RHITextureRef>) {
+                    //do texture stuff
+                } else {
+                    static_assert(false, "not a texture type");
+                }
+            } else if constexpr (std::is_same_v<cpp_type, BufferArg>) {
+                //do buffer stuff
+            }
+        }
+
+        template<typename... T, std::size_t... Is>
+        void SetParams(std::index_sequence<Is...>, T&&... _args) {
+            (..., SetParam<T, std::tuple_element_t<Is, tuple_helper>>((std::forward<T>(_args))));
         }
     };
 
     class ShaderPipeline {
 
-        ShaderArgType(RHITextureRef, Texture0)
-            ShaderArgType(RHIBufferRef, Buffer0)
-            // BEGIN_SHADER_PARAMS
-            // DEFINE_SHADER_TEXTURE(Texture0)
-            // DEFINE_SHADER_BUFFER(Buffer0)
-            // END_SHADER_PARAMS
-            using InnerShaderArgs = ShaderArgs<Texture0, Buffer0>;
+        DEFINE_SHADER_BUFFER(Buffer0);
+        DEFINE_SHADER_TEX(Texture0);
+
+        DEFINE_SHADER_ARGS(Buffer0, Texture0);
 
         template<typename T>
         void SetParam(T&& _param) {
@@ -77,7 +107,20 @@ namespace Moer {
         template<typename... T>
         void SetArgs(T&&... _args) {
             //need to make sure the args are in the right order with InnerShaderArgs
+            InnerArgs args;
+            args.SetParams(std::make_index_sequence<sizeof...(T)>(), std::forward<T>(_args)...);
         }
+
+        template<size_t... idx>
+        void InitHashArray(std::index_sequence<idx...>) {
+            hash_array = {std::tuple_element_t<idx, InnerArgs::tuple_helper>::GetHashCode()...};
+        }
+
+    public:
+        ShaderPipeline() {
+            InitHashArray(std::make_index_sequence<InnerArgs::arg_size>());
+        }
+        StaticArray<uint64_t, InnerArgs::arg_size> hash_array;
     };
 };// namespace Moer
 #endif
