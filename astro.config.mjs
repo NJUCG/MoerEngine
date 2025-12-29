12e4ffd5a931698e4ec292fdf372aae0a76f7155
Qibo0088 <241220088@smail.nju.edu.cn>
2025-12-29 22:43:42 +0800

    add: 主页源码

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // 设置基础路径为仓库名
+  site: 'https://MoerEngine.github.io',
+  base: '/gh-pages'
+});
