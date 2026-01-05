f41b4fc10fabf59974dea157249f45aa87420682
YXHXianYu <2943003@qq.com>
2026-01-05 14:08:22 +0800

    feat(ui): 在加载场景时，阻止通过UI进行其他操作

--- source/editor/EditorUI.cpp
+++ source/editor/EditorUI.cpp
@@ -239,24 +239,35 @@ void EditorUI::ShowConfig() {
 
     EditorUIStyle::ShowStyleSelector("Style##Default");
 
-    auto last_selected_render_method = m_config->selected_render_method;
-    if (ImGui::BeginCombo(
-            "Render Method", k_render_method_names[static_cast<uint>(m_config->selected_render_method)].data()
-        )) {
-        for (int i = 0; i < IM_ARRAYSIZE(k_render_method_names); i++) {
-            const bool is_selected = (m_config->selected_render_method == static_cast<ERenderMethod>(i));
-            if (ImGui::Selectable(k_render_method_names[i].data(), is_selected)) {
-                m_config->selected_render_method = static_cast<ERenderMethod>(i);
+    // MARK: Common Configs
+
+    /////////////////////////////////////////////////// Begin Disabled Here
+    // 避免场景加载一半，切换场景或渲染器，导致崩溃
+    bool is_scene_found_but_not_ready = Scene::IsSceneFound() && !Scene::IsSceneReady();
+    ImGui::BeginDisabled(is_scene_found_but_not_ready);
+
+    // Render Method
+    {
+        auto last_selected_render_method = m_config->selected_render_method;
+        if (ImGui::BeginCombo(
+                "Render Method",
+                k_render_method_names[static_cast<uint>(m_config->selected_render_method)].data()
+            )) {
+            for (int i = 0; i < IM_ARRAYSIZE(k_render_method_names); i++) {
+                const bool is_selected = (m_config->selected_render_method == static_cast<ERenderMethod>(i));
+                if (ImGui::Selectable(k_render_method_names[i].data(), is_selected)) {
+                    m_config->selected_render_method = static_cast<ERenderMethod>(i);
+                }
+                if (is_selected) {
+                    ImGui::SetItemDefaultFocus();
+                }
             }
-            if (is_selected) {
-                ImGui::SetItemDefaultFocus();
-            }
+            ImGui::EndCombo();
         }
-        ImGui::EndCombo();
-    }
-    if (last_selected_render_method != m_config->selected_render_method) {
-        m_b_need_reload = true;
-        SetShowSubUI(false);
+        if (last_selected_render_method != m_config->selected_render_method) {
+            m_b_need_reload = true;
+            SetShowSubUI(false);
+        }
     }
 
     { // Scene Path
@@ -318,6 +329,12 @@ void EditorUI::ShowConfig() {
     }
 
     ImGui::Separator();
+
+    ImGui::EndDisabled();
+    if (is_scene_found_but_not_ready) {
+        ImGui::TextColored(ImVec4(1.0f, 0.4f, 0.4f, 1.0f), "Scene is loading... Please wait.");
+    }
+    /////////////////////////////////////////////////// End Disabled Here
 
     ImGui::Text("Infos: ");
     ImGui::Text("\tFPS: %.1f", io.Framerate);
