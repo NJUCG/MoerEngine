032381334b49abfd7c4b1c0af6cf6a6a7f7c5543
YXHXianYu <2943003@qq.com>
2026-01-07 16:19:31 +0800

    feat(3rdparty): 添加了RenderDoc API的支持，可以在引擎内截帧

---
+++ source/runtime/render/debug/RenderDocApi.cpp
@@ -0,0 +1,61 @@
+#include "RenderDocApi.h"
+
+#if WITH_RENDERDOC
+
+// 减少 Windows.h 的污染
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "log/LogSystem.h"
+
+namespace Moer::Render {
+
+static std::atomic<bool> is_requesting_capture{false};
+
+RenderDocApi& RenderDocApi::Get() {
+    static RenderDocApi instance;
+    return instance;
+}
+
+RenderDocApi::RenderDocApi() {
+    // 检查 RenderDoc 是否已经注入了进程
+    // 只有通过 RenderDoc 软件 Launch 时，DLL 才会被注入
+    HMODULE mod = GetModuleHandleA("renderdoc.dll");
+
+    if (mod) {
+        pRENDERDOC_GetAPI get_api = (pRENDERDOC_GetAPI)GetProcAddress(mod, "RENDERDOC_GetAPI");
+        if (get_api) {
+            // 获取 1.1.2 版本的指针
+            get_api(eRENDERDOC_API_Version_1_1_2, (void**)&m_api);
+        }
+
+        LOG_INFO("RenderDoc API initialized.");
+    } else {
+        LOG_WARNING("RenderDoc isn't running. RenderDoc API not initialized.");
+    }
+}
+
+void RenderDocApi::StartCapture() {
+    if (m_api && !is_requesting_capture) {
+        is_requesting_capture = true;
+        m_api->StartFrameCapture(nullptr, nullptr);
+        LOG_INFO("RenderDoc frame capture started.");
+    }
+}
+
+void RenderDocApi::EndCapture() {
+    if (m_api && is_requesting_capture) {
+        m_api->EndFrameCapture(nullptr, nullptr);
+        is_requesting_capture = false; // 捕获完成，重置状态
+        LOG_INFO("RenderDoc frame capture ended.");
+    }
+}
+
+bool RenderDocApi::IsActive() const {
+    return m_api != nullptr;
+}
+
+} // namespace Moer::Render
+
+#endif // WITH_RENDERDOC
--- template.EnableCuda.cmake
+++ template.EnableCuda.cmake
@@ -6,6 +6,6 @@ set(TENSORRT_DIR "/path/to/tensor_rt" CACHE PATH "TENSORRT_DIR" FORCE)
 # WITH_CUDA 只支持 Windows11 + Vulkan
 # CMake的Cache变量会持续存在，直到删除build目录；FORCE用于覆盖之前的值
 # 启用：
-#set(WITH_CUDA ON CACHE BOOL "WITH_CUDA" FORCE)
+set(WITH_CUDA ON CACHE BOOL "WITH_CUDA" FORCE)
 # 关闭：
-set(WITH_CUDA OFF CACHE BOOL "WITH_CUDA" FORCE)
+# set(WITH_CUDA OFF CACHE BOOL "WITH_CUDA" FORCE)
